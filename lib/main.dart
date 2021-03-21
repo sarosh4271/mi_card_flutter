fab54f77ccdf388e204f86b5dffb199571387054
sarosh4271 <74011881+sarosh4271@users.noreply.github.com>
2021-03-21 19:14:51 +0500

    personal-info-card

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,83 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
+        backgroundColor: Colors.lightBlueAccent,
+        body: SafeArea(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            CircleAvatar(
+              radius: 50,
+              backgroundImage: AssetImage('image/mypic.jpeg'),
+            ),
+            Text(
+              'sarosh tahir',
+              style: TextStyle(
+                  fontFamily: 'OleoScript',
+                  fontSize: 40,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold),
+            ),
+            Text(
+              'FLUTTER DEVELOPER',
+              style: TextStyle(
+                  fontFamily: 'OleoScript',
+                  fontSize: 20,
+                  letterSpacing: 2.5,
+                  fontWeight: FontWeight.bold),
+            ),
+            SizedBox(
+              height: 10,
+              width: 100,
+              child: Divider(
+                color: Colors.black,
+              ),
+            ),
+            Card(
+              margin: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+              child: ListTile(
+                leading: Icon(
+                  Icons.phone,
+                  color: Colors.lightBlueAccent,
+                ),
+                title: Text(
+                  '+92 303 4277385',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontFamily: 'OleoScript',
+                  ),
+                ),
+              ),
+            ),
+            Card(
+              margin: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+              child: ListTile(
+                leading: Icon(
+                  Icons.email,
+                  color: Colors.lightBlueAccent,
+                ),
+                title: Text(
+                  'sarosh4271@gmail.com',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontFamily: 'OleoScript',
+                  ),
+                ),
+              ),
+            )
+          ],
+        )),
       ),
-    ),
-  );
+    );
+  }
 }
